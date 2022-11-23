1f03511359096e5701168aa08f9675e7ed04f375
Hesham-BurawiS <hesham@burawi.com>
2022-11-23 13:41:07 +0000

    test file for functions folder

---
+++ functions/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hesham Burawi on 23/11/2022.
+//
+
+#include "test.h"
---
+++ functions/test.h
@@ -0,0 +1,14 @@
+//
+// Created by Hesham Burawi on 23/11/2022.
+//
+
+#ifndef ROCKET_OS_TEST_H
+#define ROCKET_OS_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //ROCKET_OS_TEST_H
