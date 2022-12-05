86241c2308fd978f694277762d684c5bee1d1f56
Taaha <ahnafkhalique@hotmail.com>
2022-12-05 19:23:15 +0000

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+enum blinks {
+  PROGRAM_MODE = 100,
+  DEPLOYMENT = 1000
+};
+
+int led = 13;
+
+blinks b;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  b = DEPLOYMENT;
+  digitalWrite(led, HIGH);
+  delay(b);
+  digitalWrite(led, LOW);
+  delay(b);
 }
