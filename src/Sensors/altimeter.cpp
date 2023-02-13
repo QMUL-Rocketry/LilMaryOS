b8adc2fab8b40c21d63ab275d7cc5b438ed57ad3
NathanAfsar <115822709+NathanAfsar@users.noreply.github.com>
2023-02-13 12:24:04 +0000

    Update altimeter.cpp

--- src/Sensors/altimeter.cpp
+++ src/Sensors/altimeter.cpp
@@ -1,4 +1,24 @@
 #include "altimeter.h"
+#include <Adafruit_BMP280.h>
+
+Adafruit_BMP280 bmp; // I2C Interface
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("BMP280 test"));
+
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    while (1);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
 
 Altimeter::Altimeter(){
     bmp.begin();
